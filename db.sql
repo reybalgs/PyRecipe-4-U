1b3c898372f6af62997d4759243d2755b5c78d07
Aldo Rey Balagulan <mrawesomer34@gmail.com>
2012-05-06 13:58:25 +0800

    Initial commit

---
+++ db.sql
@@ -0,0 +1,7 @@
+-- Drop the database if it exists already
+IF EXISTS DROP DATABASE RecipeDatabase
+
+-- Now we shall create the tables
+
+CREATE TABLE Ingredients -- Ingredients database
+-- Inserting into ingredients database
---
+++ main.py
@@ -0,0 +1,14 @@
+###############################################################################
+#
+# PyRecipe-4-U
+#
+# A cooking recipe management app, running on the PySide Qt Framework and
+# written in Python
+#
+# Author: Aldo Rey Balagulan
+#
+###############################################################################
+
+# main.py
+#
+# Main program/file. Run this to run the entire program.
